aa4574089d07630d3b9dd1f9ce26ce44aee477a0
damianrein <damianrein464@gmail.com>
2024-11-16 15:36:03 -0300

    -

---
+++ src/main/java/com/ApiExchangeRates/ApiExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package com.ApiExchangeRates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExchangeRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExchangeRatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiExchangeRates/ApiExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiExchangeRates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
